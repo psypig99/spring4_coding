68888f16d48fa9b7d3533656fed1623bd83750fc
HongSejun <psypig99@gmail.com>
2015-12-29 18:13:11 +0900

    ch9 start

--- spring4_ch9/src/test/java/com/apress/isf/spring/test/MyDocumentsJDBCEmbeddedAnnotatedTest.java
+++ spring4_ch9/src/test/java/com/apress/isf/spring/test/MyDocumentsJDBCEmbeddedAnnotatedTest.java
@@ -1,8 +1,19 @@
 package com.apress.isf.spring.test;
 
+import com.apress.isf.java.model.Document;
+import com.apress.isf.java.model.Type;
+import com.apress.isf.java.service.SearchEngine;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Sejun on 2015. 12. 29..
@@ -10,4 +21,22 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:META-INF/spring/mydocuments-jdbc-embedded-context.xml")
 public class MyDocumentsJDBCEmbeddedAnnotatedTest {
+	@Autowired
+	private SearchEngine engine;
+	private Type webType = new Type("WEB",".url");
+
+	@Test
+	public void testJDBCEmbedded() {
+
+		List<Document> documents = engine.findByType(webType);
+		assertNotNull(documents);
+		assertTrue(documents.size() == 1);
+		assertEquals(webType.getName(), documents.get(0).getType().getName());
+		assertEquals(webType.getExtension(),
+				documents.get(0).getType().getExtension());
+
+		documents = engine.listAll();
+		assertNotNull(documents);
+		assertTrue(documents.size() == 4);
+	}
 }
